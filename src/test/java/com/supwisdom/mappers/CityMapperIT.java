9978e84ac13536c49a6310c11b04a1393194bdad
Daniel Qian <chanjarster@gmail.com>
2017-09-25 18:08:06 +0800

    polish test code

--- src/test/java/com/supwisdom/mappers/CityMapperIT.java
+++ src/test/java/com/supwisdom/mappers/CityMapperIT.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 @SpringBootTest(classes = MybatisTestConfiguration.class)
 public class CityMapperIT extends AbstractTestNGSpringContextTests {
 
@@ -15,7 +17,8 @@ public class CityMapperIT extends AbstractTestNGSpringContextTests {
   @Test
   public void testFindByState() throws Exception {
 
-    cityMapper.findByState("NY");
+    assertEquals(cityMapper.findByState("NY").size(), 1);
+
   }
 
 }
